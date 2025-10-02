a96dbc39a12b865fd7689c52ceec9c2485e2bcce
GozzarW <wgosling@protonmail.com>
2025-10-02 15:39:07 +0100

    Final dynamic array API information added

--- dynamicArray.c
+++ dynamicArray.c
@@ -1,23 +1,25 @@
 #include "dynamicArray.h"
 #include <stdio.h>
 
-
 DynamicArrayT* dynamic_array_create(size_t initial_capacity){
 
     DynamicArrayT* array = malloc(sizeof(DynamicArrayT));
     if(array == NULL){
         return NULL;
     }
-    int* data = malloc(sizeof(int) * initial_capacity);
-    if(data == NULL){
-        free(array);
-        return NULL;
-    }
 
+    if(initial_capacity == 0){
+        array->data = NULL;
+    } else {
+        array->data = malloc(sizeof(int) * initial_capacity);
+        if(array->data == NULL){
+            free(array);
+            return NULL;
+        }
+    }
 
     array->size = 0;
     array->capacity = initial_capacity;
-    array->data = data;
 
     return array;
 }
--- dynamicArray.h
+++ dynamicArray.h
@@ -1,3 +1,5 @@
+#ifndef DYNAMIC_ARRAY_H
+#define DYNAMIC_ARRAY_H
 #include <stdio.h>
 
 typedef struct DynamicArray{
@@ -6,9 +8,19 @@ typedef struct DynamicArray{
     size_t capacity;
 }DynamicArrayT;
 
+// Returns pointer to the array, NULL if memory allocation fails
 DynamicArrayT* dynamic_array_create(size_t initial_capacity);
+
 void dynamic_array_destroy(DynamicArrayT* array);
+// Returns 0 on success, -1 if the growing array fails
 int dynamic_array_append(DynamicArrayT* array, int value);
+// Returns 0 on success, -1 if the index is out of bounds
 int dynamic_array_get(DynamicArrayT* array, size_t index, int* out_value);
+// Returns 0 on success, -1 if the index is out of bounds
 int dynamic_array_set(DynamicArrayT* array, size_t index, int value);
+// Returns 0 on success, -1 if the index is out of bounds
 int dynamic_array_remove(DynamicArrayT* array, size_t index);
+// Returns 0 on success, -1 if shrinking array fails
+int dynamic_array_shrink(DynamicArrayT* array, int new_capacity);
+
+#endif
