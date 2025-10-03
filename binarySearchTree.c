d5d22da194f3884d34e9946438501efd15397e34
GozzarW <wgosling@protonmail.com>
2025-10-03 12:17:21 +0100

    BST remove function

--- binarySearchTree.c
+++ binarySearchTree.c
@@ -83,3 +83,93 @@ BSTNode* create_node(int value){
     node->right = NULL;
     return node;
 }
+
+int bst_search(BSTree* tree, int value){
+
+    if(tree == NULL || tree->root == NULL){
+        return -1;
+    }
+    
+    BSTNode* current = tree->root;
+
+    while(current != NULL){
+        if(current->data == value){
+            return 1;
+        }
+        if(value < current->data){
+            current = current->left;
+        } else {
+            current = current->right;
+        }
+    }
+    return 0;
+}
+
+int bst_remove(BSTree* tree, int value){
+
+    if(tree == NULL || tree->root == NULL){
+        return -1;
+    }
+
+    BSTNode* parent = NULL;
+    BSTNode* current = tree->root;
+    
+    while(current != NULL && current->data != value){
+        parent = current;
+        if(value < current->data){
+            current = current->left;
+        }else{
+            current = current->right;
+        }
+    }
+
+    if(current == NULL){
+        return 0;
+    }
+
+    if(current->left == NULL && current->right == NULL){
+        if(parent == NULL){
+            tree->root = NULL;
+        }else if(parent->left == current){
+            parent->left = NULL;
+        } else{
+            parent->right = NULL;
+        }
+        free(current);
+        tree->size--;
+        return 0;
+    }
+    else if(current->left != NULL ^ current->right != NULL){
+        BSTNode* child = (current->left != NULL) ? current->left : current->right;
+        if(parent == NULL){
+            tree->root = child;
+        } else if(parent->left == current){
+              parent->left = child;
+        } else{
+                parent->right = child;
+            }
+        free(current);
+        tree->size--;
+        return 0;
+    } else {
+        BSTNode* successor_parent = current;
+        BSTNode* successor = current->right;
+
+        while(successor->left != NULL){
+            successor_parent = successor;
+            successor = successor->left;
+        }
+
+        current->data = successor->data;
+
+        if(successor_parent->left == successor){
+            successor_parent->left = successor->right;
+        } else {
+            successor_parent->right = successor->right;
+        }
+        free(successor);
+        tree->size--;
+        return 0;
+    }
+    return -1;
+}
