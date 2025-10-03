c1d23e7e94fb66eca73cfd35381b8bbf28a03c72
GozzarW <wgosling@protonmail.com>
2025-10-03 12:22:47 +0100

    BST header file documentation added

--- binarySearchTree.h
+++ binarySearchTree.h
@@ -16,9 +16,15 @@ typedef struct BSTree{
     size_t size;
 } BSTree;
 
-
+// Returns an empty binary search tree 
 BSTree* bst_create();
+
 void bst_destroy(BSTree* tree);
+// Returns 0 on success, -1 if NULL parameter parsed or failed memory allocation
 int bst_insert(BSTree* tree, int value);
+// Returns 1 if value found, 0 if failed to find value, -1 if NULL parameter parsed
+int bst_search(BSTree* tree, int value);
+// Returns 0 if value is removed, -1 if NULL parameter parsed or failed to remove value
+int bst_remove(BSTree* tree, int value);
 
 #endif
